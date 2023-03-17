87aa5c208af6eb6086058cd706d399ff435ea89d
Sann01 <eduardo-jsdsantos@educar.rs.gov.br>
2023-03-17 11:32:44 -0300

    parte 3

--- JS/script.js
+++ JS/script.js
@@ -34,10 +34,34 @@ btnBuscarFilme.onclick= () => {
     let listarFilmes = async (filmes) => { 
     let listaFilmes = await document.querySelector("#lista-filmes"); 
     listaFilmes.innerHTML = "";
-    console.log(listaFilmes);
+   //console.log(listaFilmes);
     if (filmes.length > 0) {
     filmes.forEach(async (filme) => { 
     listaFilmes.appendChild (await filme.getCard());
+    filme.getBtnDetalhes().onclick = () =>  {
+        detalhesFilme(filme.id);
+    }
     });
     }
+}
+    let detalhesFilme = async (id) => {
+        fetch("https://www.omdbapi.com/?&apikey=768c9c1&i="+id)
+        .then((resp) => resp.json())
+        .then((resp) => {
+            console.log(resp);
+            let filme=new Filme(
+                resp.imdbID,
+                resp.Title,
+                resp.Year,
+                resp.Genre.split(","),
+                resp.Runtime,
+                resp.Poster,
+                resp.plot,
+                resp.Director,
+                resp.Actors.split(","),
+                resp.Awards,
+                resp.imdbRating
+            )
+            console.log(filme);
+     })
 }
